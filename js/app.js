b8666b29899224b123946f92127818472a10fee4
ShiCheng Lu <shichenglu0708@gmail.com>
2023-08-08 17:46:05 -0400

    fix document issue

--- js/app.js
+++ js/app.js
@@ -14,8 +14,8 @@ let models = [];
 const mixers = [];
 const clock = new THREE.Clock();
 
-function init(){
-   container = document.querySelector( '#scene-container' );
+function init() {
+   container = document.querySelector('#scene-container');
 
    createScene();
    createCamera();
@@ -31,86 +31,84 @@ function init(){
    // /
 }
 
-function createScene(){
+function createScene() {
    scene = new THREE.Scene();
-   scene.background = new THREE.Color( 0x8FBCD4 );
+   scene.background = new THREE.Color(0x8FBCD4);
 }
-function createCamera(){
-   camera = new THREE.PerspectiveCamera( 35, container.clientWidth / container.clientHeight, 1, 3000);
+function createCamera() {
+   camera = new THREE.PerspectiveCamera(35, container.clientWidth / container.clientHeight, 1, 3000);
    // camera.position.set( -1.5, 1.5, 6.5);
-   camera.position.set( 0, 0, 300);
+   camera.position.set(0, 0, 300);
 }
-function createControls(){
-   controls = new THREE.OrbitControls( camera, container);
-   
-   dragControls = new THREE.DragControls( models, camera, container );
-   dragControls.addEventListener( 'dragstart', function(event){
-      console.log( 'drag start' );
+function createControls() {
+   controls = new THREE.OrbitControls(camera, container);
+
+   dragControls = new THREE.DragControls(camera, container);
+   dragControls.addEventListener('dragstart', function (event) {
+      console.log('drag start');
       controls.enabled = false;
    });
-   dragControls.addEventListener( 'drag', function(event){
-      console.log( 'drag' );
+   dragControls.addEventListener('drag', function (event) {
+      console.log('drag');
    });
-   dragControls.addEventListener( 'dragend', function(event){
-      console.log( 'drag end' );
+   dragControls.addEventListener('dragend', function (event) {
+      console.log('drag end');
       controls.enabled = true;
    });
 }
 
 // _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
 
-function createLights(){
-   const hemisphereLight = new THREE.HemisphereLight( 0xddeeff, 0x202020, 5.0);
-   const mainLight = new THREE.DirectionalLight( 0xFFFFFF, 5.0);
-   mainLight.position.set( 10, 10, 10);
-   scene.add( hemisphereLight, mainLight);
+function createLights() {
+   const hemisphereLight = new THREE.HemisphereLight(0xddeeff, 0x202020, 5.0);
+   const mainLight = new THREE.DirectionalLight(0xFFFFFF, 5.0);
+   mainLight.position.set(10, 10, 10);
+   scene.add(hemisphereLight, mainLight);
 }
 
 // _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
-function loadModel(){
+function loadModel() {
    const loader = new THREE.GLTFLoader();
 
-   const onLoad = ( gltfResult, position ) => {
-      model = gltfResult.scene.children[ 0 ];
+   const onLoad = (gltfResult, position) => {
+      model = gltfResult.scene.children[0];
 
-      model.position.copy( position );
+      model.position.copy(position);
 
-      scene.add( model );
-      models.push( model );
+      scene.add(model);
+      models.push(model);
    };
 
-   const onProgress = () => { console.log( 'Models are on the way...' ); };
+   const onProgress = () => { console.log('Models are on the way...'); };
    //
-   const onError = ( errorMessage ) => { console.log( errorMessage ); };
+   const onError = (errorMessage) => { console.log(errorMessage); };
 
-   const myFilePath = document.getElementById("myFilePath").value;
-
-   const parrotPosition = new THREE.Vector3( 0, 0, 0);
-   loader.load( 'computer.glb' , gltfResult => onLoad( gltfResult, parrotPosition ), onProgress, onError );
+   const parrotPosition = new THREE.Vector3(0, 0, 0);
+   loader.load('computer.glb', gltfResult => onLoad(gltfResult, parrotPosition), onProgress, onError);
 }
 
 
 // _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
 
-function createRenderer(){
-   renderer = new THREE.WebGLRenderer( {antialias:true, container} );
-   renderer.setSize( container.clientWidth, container.clientHeight);
-   renderer.setPixelRatio( window.devicePixelRatio);
-   container.appendChild( renderer.domElement);
+function createRenderer() {
+   renderer = new THREE.WebGLRenderer({ antialias: true, container });
+   renderer.setSize(container.clientWidth, container.clientHeight);
+   renderer.setPixelRatio(window.devicePixelRatio);
+   container.appendChild(renderer.domElement);
 }
 
-function render(){
-   renderer.render( scene, camera);
+function render() {
+   renderer.render(scene, camera);
 }
 
-function onWindowResize(){
+function onWindowResize() {
    // set the aspect ratio to match the new browser window aspect ratio
    camera.aspect = container.clientWidth / container.clientHeight;
    // update the camera's frustrum
    camera.updateProjectionMatrix();
    // update the size of the renderer and the canvas
-   renderer.setSize( container.clientWidth, container.clientHeight);
+   renderer.setSize(container.clientWidth, container.clientHeight);
 }
-window.addEventListener( 'resize', onWindowResize);
+window.addEventListener('resize', onWindowResize);
 
 init();
