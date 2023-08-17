550ba7cee2f8a78428d8f4130d1ab293300d3f9f
ShiCheng Lu <shichenglu0708@gmail.com>
2023-08-16 23:51:28 -0400

    fix missing redstone

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ from instruction_set import *
 
 # multiply two number
 program = '''
-li $1 14
-li $0 14
-mul $0 $1
+li $0 3     #
+str $0 10    #
+li $0 1     #
+str $0 8    #
+ldr $0 10    #
+ldr $1 8    #
 hlt
 '''
 
 if __name__ == "__main__":
-    # auto_program(program)
-    sim(program)
+    auto_program(instruction_set.fib_program)
+    # sim(program)
     # print(bin(int("1111111", base=2) * int("11111111", base=2)))
 
     pass
